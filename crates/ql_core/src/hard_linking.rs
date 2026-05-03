629f14089869a67afd158c2b64da9dc14588d970
Void98 <shadow_pie99@proton.me>
2026-05-03 13:32:51 +0200

    removed unnecessary code

--- crates/ql_core/src/hard_linking.rs
+++ crates/ql_core/src/hard_linking.rs
@@ -9,28 +9,10 @@ async fn create_hard_links(links: Vec<(&Path, &Path)>) -> Result<(), IoError> {
     Ok(())
 }
 
-pub async fn file_linking() {
-    /*
-    TODO: This might help you do the frontend
-
-    let file_type;
-    //TODO: ADD THE FILE TYPE LIKE: resourcepack, config, shaderpack .
-    let file_names;
-    //TODO: ADD THE FILE NAMES
-
-    let selected_global_source_path = LAUNCHER_DIR.join("global_resources").join(file_type);
-    let instance_path:Path = "current instance path";
-    // TODO: add the selected instance here ^^^^^^^
-    let instance_resource_path;
-
-    if file_type == "config" { instance_resource_path = instance_path.join(".minecraft") }
-    else { instance_resource_path = instance_path.join(".minecraft").join(file_type) }
-
-     */
-
-    let sources: Vec<&Path> = vec![];
-    let destinations: Vec<&Path> = vec![];
-    // TODO: THESE NEED TO BE ABSOLUTE PATHS LIKE: sources -> ("SOURCE/FILE/PATH.zip", "SOURCE/FILE/PATH2.zip") dest -> ("DEST/FILE/PATH.zip", "DEST/FILE/PATH2.zip")
+pub async fn file_linking(
+    sources: Vec<&Path>,
+    destinations: Vec<&Path>
+    ){
 
     let links: Vec<(&Path, &Path)> = sources
         .iter()
@@ -40,5 +22,5 @@ pub async fn file_linking() {
 
     create_hard_links(links)
         .await
-        .expect("Unable to create links");
+        .unwrap();
 }
